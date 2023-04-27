a6e5ef38b76245669eb1e79cad576c803deabdd2
Nyadzayo <kelvin.nyadzayo16@gmail.com>
2023-04-27 12:07:33 +0200

    CNF-01 built the swap landing page front end

--- dex/src/components/Header.js
+++ dex/src/components/Header.js
@@ -1,8 +1,32 @@
 import React from 'react'
-
+import Logo from "../moralis-logo.svg";
+import Eth from "../eth.svg";
+import {Link} from "react-router-dom"
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className="leftH">
+        <img src={Logo} alt="Logo" className='logo'/>
+          <Link to="/" className="link">
+             <div className="headerItem">Swap</div>
+          </Link>
+          <Link to="/tokens" className="link">
+               <div className="headerItem">Tokes</div>
+          </Link>
+        
+      </div>
+      <div className="rightH">
+        <div className="headerItem">
+          <img src={Eth} alt="Eth" className='eth'/>
+          Ethereum
+        </div>
+        <div className="connnectButton">
+          Connect
+        </div>
+
+
+      </div>
+    </header>
   )
 }
 
--- dex/src/components/Swap.js
+++ dex/src/components/Swap.js
@@ -1,8 +1,131 @@
-import React from 'react'
+import React, {useState, useEffect}from 'react'
+import { Input, Popover , Radio, Modal, message} from 'antd';
+import {ArrowDownOutlined , DownOutlined, SettingOutlined } from "@ant-design/icons";
+import tokenList  from "../tokenList.json"
 
 function Swap() {
+  const [slippage , setSlippage] = useState(2.5);
+  const [tokenOneAmount, setTokenOneAmount] = useState(null);
+  const [tokenTwoAmount, setTokenTwoAmount] = useState(null);
+  const [tokenOne, setTokenOne] = useState(tokenList[0]);
+  const [tokenTwo, setTokenTwo] = useState(tokenList[1]);
+  const [isOpen, setIsOpen] = useState(false)
+  const [changeToken, setChangeToken] = useState(1);
+
+
+
+  const handleSlippageChange = (e) =>{
+      setSlippage(e.target.value)
+  }
+  const onChangeToken  = (e) =>{
+    setTokenOneAmount(e.target.value)
+
+  }
+  const onSwitchToken = ()=>{
+    const one = tokenOne;
+    const two = tokenTwo;
+    setTokenOne(two);
+    setTokenTwo(one);
+  }
+  const openModal = (assetNumber)=>{
+    setChangeToken(assetNumber)
+    setIsOpen(true)
+  }
+  const modifyToken = (token) =>{
+    if(changeToken===1){
+      setTokenOne(tokenList[token])
+    }else{
+      setTokenTwo(tokenList[token])
+    }
+    setIsOpen(false)
+
+  }
+  const fetchDexSwap = () =>{
+    
+  }
+  const settings = (
+    <>
+    <div>Slipage Tolerance</div>
+       <div>
+            <Radio.Group value={slippage} onChange={handleSlippageChange}>
+                <Radio.Button  value={0.5}>0.5%</Radio.Button>
+                <Radio.Button value={2.5}>2.5%</Radio.Button>
+                <Radio.Button value={3.5}>3.5%</Radio.Button>
+            </Radio.Group>
+    </div>
+    </>
+
+  );
   return (
-    <div>Swap</div>
+    <>
+    <Modal
+       open={isOpen}
+       footer={null}
+       onCancel={()=>setIsOpen(false)}
+       title="Select a token"
+    
+    >
+      <div className="modaContent">
+        {
+          tokenList?.map((e,i)=>{
+            return(
+              <div 
+              className="tokenChoice"
+              key={i}
+              onClick={()=>modifyToken(i)}              
+              >
+                <img src={e.img} alt={e.ticker} className='assetLogo'/>
+                <div className="tokenChoiceName">
+                  <div className='tokenName'>{e.name}</div>
+                  <div className='tokenTicker' >{e.ticker} </div>
+                </div>
+              </div>
+            )
+          })
+        }
+
+
+      </div>
+
+    </Modal>
+     <div className="tradeBox">
+      <div className="tradeBoxHeader"> 
+          <h4>Swap</h4>
+          <Popover
+           content={settings}
+           title="Settings"
+           trigger="click"
+           placement="buttomRight"
+            >
+          <SettingOutlined className='cog'/>
+          </Popover>
+         
+      
+      </div>
+      <div className="inputs">
+            <Input placeholder="0" value={tokenOneAmount}  onChange={onChangeToken} />
+            <Input placeholder="0" value={tokenTwoAmount}  disabled='true'  />
+            <div className="assetOne" onClick={()=>openModal(1)}>
+              <img src={tokenOne.img} alt="assetOneLogo" className="assetLogo"/>
+              {tokenOne.ticker}
+              <DownOutlined/>
+            </div>
+            <div className="switchButton" onClick={onSwitchToken}>
+              <ArrowDownOutlined className="switchArrow"/>
+            </div>
+            <div className="assetTwo" onClick={()=>openModal(2)}>
+            <img src={tokenTwo.img} alt="assetTwoLogo" className="assetLogo"/>
+            {tokenTwo.ticker}
+              <DownOutlined/>
+            </div>
+          </div>
+
+       <div className="swapButton" onClick={fetchDexSwap} disabled={!tokenOneAmount}>Swap</div>
+
+
+    </div>
+    </>
+   
   )
 }
 
